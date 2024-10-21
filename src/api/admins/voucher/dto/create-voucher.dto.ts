82844195596c18887de9617b441e715dc629c686
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-10-21 10:04:42 +0700

    udpate voucher

--- src/api/admins/voucher/dto/create-voucher.dto.ts
+++ src/api/admins/voucher/dto/create-voucher.dto.ts
@@ -1,1 +1,36 @@
-export class CreateVoucherDto {}
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateVoucherDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  code: string;
+
+  @IsString()
+  paymentMethod: string;
+
+  @IsNumber()
+  discount: number;
+
+  @IsString()
+  typeDiscount: string;
+
+  @IsNumber()
+  discountToUp: number;
+
+  @IsOptional()
+  @IsNumber()
+  minimumOrder: number;
+
+  @IsOptional()
+  @IsNumber()
+  quantity: number;
+
+  @IsOptional()
+  @IsString()
+  icon: string;
+}
